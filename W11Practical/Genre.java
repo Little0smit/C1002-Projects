38d529029e8ac067e4be399ea05b333768d4a33a
Little0smit <Little0smit@users.noreply.github.com>
2015-11-27 10:48:23 +0000

    Create Genre.java

---
+++ W11Practical/Genre.java
@@ -0,0 +1,19 @@
+/**
+ * Created by ds221 on 26/11/15.
+ */
+public class Genre {
+    private String name;
+
+    Genre(String name){
+        this.name = name;
+    }
+    public Genre() {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
